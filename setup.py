d2b6e12a442fe68044475138ab8e42d8efcc9a48
Michael Duncan <msduncan82@gmail.com>
2020-05-04 13:54:16 -0600

    init commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A simple wrapper to interact with web apis',
+    author='Michael Duncan',
+    license='MIT',
+)
